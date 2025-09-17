a372a4f3efdccc1bba48796454bac2f0bdef57e0
RithmaRandima <rithma.r.nandasiri@gmail.com>
2025-09-17 21:25:58 +0530

    completed Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import { GiHamburgerMenu } from "react-icons/gi";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="absolute top-0 left-0 w-full pt-10 text-white z-20">
+      <div className="  ">
+        <div className="flex items-center justify-between">
+          {/* logo section */}
+          <h1 className="tezt-2xl font-semibold uppercase">
+            COdes <span className="text-orange-300">Coffee</span>
+          </h1>
+          {/* ham menu section*/}
+          <div>
+            <GiHamburgerMenu className="text-3xl cursor-pointer" />
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
